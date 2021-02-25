2e7aec4f684d1b19609e6e556538dcdb5f8e4fe7
Ananda Fidelis <anandacfesilva@recife.pe.senac.br>
2021-02-25 20:43:18 -0300

    alteração

--- src/routes/indexRT.js
+++ src/routes/indexRT.js
@@ -4,7 +4,7 @@ var formacao = require('./formacao.json')
 var experiencia = require('./experiencia.json')
 var idiomas = require('./idiomas.json')
 
-console.log(experiencia);
+console.log(pessoa);
 module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render('index', { pessoa, skills, formacao, experiencia, idiomas
